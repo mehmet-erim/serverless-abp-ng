db9269adbdb84304bb040bb46f5cac966704c11a
mehmet-erim <bilgi@mehmeterim.com>
2020-04-24 00:51:00 +0300

    feat: install abp packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConfirmationService } from '@abp/ng.theme.shared';
+import { LazyLoadService, LOADING_STRATEGY } from '@abp/ng.core';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'serverless-abp-ng';
+export class AppComponent implements OnInit {
+  constructor(
+    private confirmation: ConfirmationService,
+    private lazyLoadService: LazyLoadService
+  ) {}
+
+  ngOnInit() {
+    forkJoin([
+      this.lazyLoadService.load(
+        LOADING_STRATEGY.PrependAnonymousStyleToHead(
+          'fontawesome-v4-shims.min.css'
+        )
+      ),
+      this.lazyLoadService.load(
+        LOADING_STRATEGY.PrependAnonymousStyleToHead('fontawesome-all.min.css')
+      ),
+    ]).subscribe();
+  }
+  openConfirmation() {
+    this.confirmation.warn('This entity will be deleted!', 'Are you sure?');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { CoreModule } from '@abp/ng.core';
+import { ThemeSharedModule } from '@abp/ng.theme.shared';
+import { ThemeBasicModule } from '@abp/ng.theme.basic';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxsModule.forRoot(),
+    CoreModule.forRoot({ environment: {}, skipGetAppConfiguration: true }),
+    ThemeSharedModule.forRoot(),
+    ThemeBasicModule,
+
+    NgxsLoggerPluginModule.forRoot({ disabled: environment.production }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
